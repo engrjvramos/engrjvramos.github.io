89cc8015b3f9787c32bba3f5edc09929aa21fc74
Jose Ramos <engrjvramos@gmail.com>
2022-01-31 18:36:10 +0800

    added functionality and media queries

--- script/script.js
+++ script/script.js
@@ -1,3 +1,31 @@
+// NAVIGATION MENU ITEMS ACTIVE ON PAGE SCROLL
+window.addEventListener('scroll', () => {
+	const sections = document.querySelectorAll('section');
+	const scrollY = window.pageYOffset;
+
+	sections.forEach((current) => {
+		let sectionHeight = current.offsetHeight;
+		let sectionTop = current.offsetTop - 50;
+		let id = current.getAttribute('id');
+
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document
+				.querySelector('.nav-items a[href*=' + id + ']')
+				.classList.add('active');
+			document
+				.querySelector('.sidebar .nav-items a[href*=' + id + ']')
+				.classList.add('active');
+		} else {
+			document
+				.querySelector('.nav-items a[href*=' + id + ']')
+				.classList.remove('active');
+			document
+				.querySelector('.sidebar .nav-items a[href*=' + id + ']')
+				.classList.remove('active');
+		}
+	});
+});
+
 // DARK & LIGHT MODE
 const themeBtn = document.querySelector('.theme-btn');
 
@@ -23,6 +51,33 @@ if (savedTheme) {
 	);
 	themeBtn.classList[savedIcon === 'sun' ? 'add' : 'remove']('sun');
 }
+
+// HAMBURGER MENU
+const menuBtn = document.querySelector('.menu-btn');
+const sideBar = document.querySelector('.sidebar');
+const navigation = document.querySelector('.navigation');
+const navItems = document.querySelectorAll('.nav-items a');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+	if (!menuOpen) {
+		menuBtn.classList.add('open');
+		menuOpen = true;
+		sideBar.classList.add('active');
+	} else {
+		menuBtn.classList.remove('open');
+		menuOpen = false;
+		sideBar.classList.remove('active');
+	}
+});
+
+navItems.forEach((navItem) => {
+	navItem.addEventListener('click', () => {
+		menuBtn.classList.remove('open');
+		menuOpen = false;
+		sideBar.classList.remove('active');
+	});
+});
 
 // TYPE WRITER EFFECT
 const TypeWriter = function (txtElement, words, wait = 3000) {
