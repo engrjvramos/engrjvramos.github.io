7279e2be8ecfa29926b70ee59099ba0726238482
Jose Ramos <engrjvramos@gmail.com>
2022-01-31 13:31:36 +0800

    more editing

--- script/script.js
+++ script/script.js
@@ -75,4 +75,14 @@ function init() {
 	new TypeWriter(txtElement, words, wait);
 }
 
-// HOVER SKILL ICONS
+// SCROLL TO TOP BUTTON
+const scrollTopBtn = document.querySelector('.scrollToTop-btn');
+
+window.addEventListener('scroll', () => {
+	scrollTopBtn.classList.toggle('active', window.scrollY > 500);
+});
+
+scrollTopBtn.addEventListener('click', () => {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+});
